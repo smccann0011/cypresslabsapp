c968d0c190ba62de0be4e248e7d06040a863620b
Shawn McCann <smccann0011@gmail.com>
2021-12-19 21:34:40 -0800

    updates to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Amplify from 'aws-amplify';
+import {AmplifyAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+import awsconfig from './aws-exports'; // if you are using Amplify CLI
 import './App.css';
 
-// Get the aws resources configuration parameters
-import awsconfig from './aws-exports'; // if you are using Amplify CLI
-
-//import { withAuthenticator } from '@aws-amplify/ui-react';
-import { withAuthenticator } from 'aws-amplify-react-native';
-
-import Amplify from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+//import { withAuthenticator } from 'aws-amplify-react-native';
 
 Amplify.configure(awsconfig);
 
@@ -16,7 +13,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to Cypress Labs</h1>
       </header>
     </div>
@@ -25,3 +21,12 @@ function App() {
 
 //export default withAuthenticator (App, {includeGreetings: true});
 export default withAuthenticator(App);
+
+//const App = () => (
+//  <AmplifyAuthenticator>
+//    <div>
+//      My App
+//      <AmplifySignOut />
+//    </div>
+//  </AmplifyAuthenticator>
+//);
