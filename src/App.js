53c5653356e4e128bb69efb168cae154046c2e32
Shawn McCann <smccann0011@gmail.com>
2021-12-20 09:14:23 -0800

    updates to App.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import React from 'react';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports'; // if you are using Amplify CLI
 import { withAuthenticator } from '@aws-amplify/ui-react';
-//import {AmplifyAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+import {Authenticator} from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 import './App.css';
 
 Amplify.configure(awsconfig);
 
 function App() {
   return (
+  <Authenticator>
+    {({ signOut, user }) => (
     <div className="App">
+      <p>
+        Hey {user.username}, welcome to my channel, with auth!
+      </p>
       <header className="App-header">
         <h1>Welcome to Cypress Labs</h1>
       </header>
     </div>
+    )}
+    </Authenticator>
   );
 }
-
-//const App = () => (
-//  <AmplifyAuthenticator>
-//    <div>
-//      <h1>Welcome to Cypress Labs</h1>
-//      <AmplifySignOut />
-//    </div>
-//  </AmplifyAuthenticator>
-//);
 
 //export default withAuthenticator (App, {includeGreetings: true});
 export default withAuthenticator(App);
